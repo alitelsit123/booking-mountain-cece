da74d3d65604f66eab4d054bd83cf593e348a569
reza <reza.eduwork@gmail.com>
2022-09-30 23:13:34 +0700

    r

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -1,357 +1,70 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
+  <head>
+    <title>Invoice Tracker</title>
     <meta charset="utf-8">
-    <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
-    <!--  All snippets are MIT license http://bootdey.com/license -->
-    <title>simple invoice receipt email template - Bootdey.com</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
-<table class="body-wrap">
-    <tbody><tr>
-        <td></td>
-        <td class="container" width="600">
-            <div class="content">
-                <table class="main" width="100%" cellpadding="0" cellspacing="0">
-                    <tbody><tr>
-                        <td class="content-wrap aligncenter">
-                            <table width="100%" cellpadding="0" cellspacing="0">
-                                <tbody><tr>
-                                    <td class="content-block">
-                                        <h2>Thanks for using our app</h2>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="content-block">
-                                        <table class="invoice">
-                                            <tbody><tr>
-                                                <td>Anna Smith<br>Invoice #12345<br>June 01 2015</td>
-                                            </tr>
-                                            <tr>
-                                                <td>
-                                                    <table class="invoice-items" cellpadding="0" cellspacing="0">
-                                                        <tbody><tr>
-                                                            <td>Service 1</td>
-                                                            <td class="alignright">$ 20.00</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Service 2</td>
-                                                            <td class="alignright">$ 10.00</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Service 3</td>
-                                                            <td class="alignright">$ 6.00</td>
-                                                        </tr>
-                                                        <tr class="total">
-                                                            <td class="alignright" width="80%">Total</td>
-                                                            <td class="alignright">$ 36.00</td>
-                                                        </tr>
-                                                    </tbody></table>
-                                                </td>
-                                            </tr>
-                                        </tbody></table>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="content-block">
-                                        <a href="#">View in browser</a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="content-block">
-                                        Company Inc. 123 Van Ness, San Francisco 94102
-                                    </td>
-                                </tr>
-                            </tbody></table>
-                        </td>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+    @php
+      $leader = $book->members()->whereRole('leader')->first();
+      $members = $book->members()->whereRole('member')->get();
+    @endphp
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-6 col-lg-6 col-sm-12 m-auto">
+                <h3>{{$book->invoice_code}}</h3>
+                <p> Hallo, {{$leader->name}}</p>
+                <p>Pembayaran untuk Booking Gunung Budheg berhasil.</p>
+                <p>Rincian: </p>
+                <ul class="list-group">
+                  <li class="list-group-item">Tanggal Pendakian: {{$book->date}}</li>
+                  <li class="list-group-item">Jumlah Orang: {{$book->members()->count()}}</li>
+                  <li class="list-group-item">Total harga: Rp. {{number_format($book->total_price)}}</li>
+                </ul>
+                <br>
+                <h3 class="mb-4">Anggota</h3>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">Nama</th>
+                      <th scope="col">Nomor Hp</th>
+                      <th scope="col">Usia</th>
+                      <th scope="col">Berat Badan</th>
+                      <th scope="col">Status</th>
                     </tr>
-                </tbody></table>
-                <div class="footer">
-                    <table width="100%">
-                        <tbody><tr>
-                            <td class="aligncenter content-block">Questions? Email <a href="mailto:">support@company.inc</a></td>
-                        </tr>
-                    </tbody></table>
-                </div></div>
-        </td>
-        <td></td>
-    </tr>
-</tbody></table>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">{{$leader->name}}</th>
+                      <td>{{$leader->phone}}</td>
+                      <td>{{$leader->age}} Th</td>
+                      <td>{{$leader->weight}} Kg</td>
+                      <td>{{$leader->role}}</td>
+                    </tr>
+                    @foreach ($members as $row)
+                      <tr>
+                        <th scope="row">{{$row->name}}</th>
+                        <td>{{$row->phone}}</td>
+                        <td>{{$row->age}} Th</td>
+                        <td>{{$row->weight}} Kg</td>
+                        <td>{{$row->role}}</td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+                <br/>
+                <br/>
+                <br/>
+                <p>Hormat Kami,</p>
+                <p>Panitia Gunung</p>
+            </div>
+        </div>
+    </div>
 
-<style type="text/css">
-/* -------------------------------------
-    GLOBAL
-    A very basic CSS reset
-------------------------------------- */
-* {
-    margin: 0;
-    padding: 0;
-    font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
-    box-sizing: border-box;
-    font-size: 14px;
-}
-
-img {
-    max-width: 100%;
-}
-
-body {
-    -webkit-font-smoothing: antialiased;
-    -webkit-text-size-adjust: none;
-    width: 100% !important;
-    height: 100%;
-    line-height: 1.6;
-}
-
-/* Let's make sure all tables have defaults */
-table td {
-    vertical-align: top;
-}
-
-/* -------------------------------------
-    BODY & CONTAINER
-------------------------------------- */
-body {
-    background-color: #f6f6f6;
-}
-
-.body-wrap {
-    background-color: #f6f6f6;
-    width: 100%;
-}
-
-.container {
-    display: block !important;
-    max-width: 600px !important;
-    margin: 0 auto !important;
-    /* makes it centered */
-    clear: both !important;
-}
-
-.content {
-    max-width: 600px;
-    margin: 0 auto;
-    display: block;
-    padding: 20px;
-}
-
-/* -------------------------------------
-    HEADER, FOOTER, MAIN
-------------------------------------- */
-.main {
-    background: #fff;
-    border: 1px solid #e9e9e9;
-    border-radius: 3px;
-}
-
-.content-wrap {
-    padding: 20px;
-}
-
-.content-block {
-    padding: 0 0 20px;
-}
-
-.header {
-    width: 100%;
-    margin-bottom: 20px;
-}
-
-.footer {
-    width: 100%;
-    clear: both;
-    color: #999;
-    padding: 20px;
-}
-.footer a {
-    color: #999;
-}
-.footer p, .footer a, .footer unsubscribe, .footer td {
-    font-size: 12px;
-}
-
-/* -------------------------------------
-    TYPOGRAPHY
-------------------------------------- */
-h1, h2, h3 {
-    font-family: "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
-    color: #000;
-    margin: 40px 0 0;
-    line-height: 1.2;
-    font-weight: 400;
-}
-
-h1 {
-    font-size: 32px;
-    font-weight: 500;
-}
-
-h2 {
-    font-size: 24px;
-}
-
-h3 {
-    font-size: 18px;
-}
-
-h4 {
-    font-size: 14px;
-    font-weight: 600;
-}
-
-p, ul, ol {
-    margin-bottom: 10px;
-    font-weight: normal;
-}
-p li, ul li, ol li {
-    margin-left: 5px;
-    list-style-position: inside;
-}
-
-/* -------------------------------------
-    LINKS & BUTTONS
-------------------------------------- */
-a {
-    color: #1ab394;
-    text-decoration: underline;
-}
-
-.btn-primary {
-    text-decoration: none;
-    color: #FFF;
-    background-color: #1ab394;
-    border: solid #1ab394;
-    border-width: 5px 10px;
-    line-height: 2;
-    font-weight: bold;
-    text-align: center;
-    cursor: pointer;
-    display: inline-block;
-    border-radius: 5px;
-    text-transform: capitalize;
-}
-
-/* -------------------------------------
-    OTHER STYLES THAT MIGHT BE USEFUL
-------------------------------------- */
-.last {
-    margin-bottom: 0;
-}
-
-.first {
-    margin-top: 0;
-}
-
-.aligncenter {
-    text-align: center;
-}
-
-.alignright {
-    text-align: right;
-}
-
-.alignleft {
-    text-align: left;
-}
-
-.clear {
-    clear: both;
-}
-
-/* -------------------------------------
-    ALERTS
-    Change the class depending on warning email, good email or bad email
-------------------------------------- */
-.alert {
-    font-size: 16px;
-    color: #fff;
-    font-weight: 500;
-    padding: 20px;
-    text-align: center;
-    border-radius: 3px 3px 0 0;
-}
-.alert a {
-    color: #fff;
-    text-decoration: none;
-    font-weight: 500;
-    font-size: 16px;
-}
-.alert.alert-warning {
-    background: #f8ac59;
-}
-.alert.alert-bad {
-    background: #ed5565;
-}
-.alert.alert-good {
-    background: #1ab394;
-}
-
-/* -------------------------------------
-    INVOICE
-    Styles for the billing table
-------------------------------------- */
-.invoice {
-    margin: 40px auto;
-    text-align: left;
-    width: 80%;
-}
-.invoice td {
-    padding: 5px 0;
-}
-.invoice .invoice-items {
-    width: 100%;
-}
-.invoice .invoice-items td {
-    border-top: #eee 1px solid;
-}
-.invoice .invoice-items .total td {
-    border-top: 2px solid #333;
-    border-bottom: 2px solid #333;
-    font-weight: 700;
-}
-
-/* -------------------------------------
-    RESPONSIVE AND MOBILE FRIENDLY STYLES
-------------------------------------- */
-@media only screen and (max-width: 640px) {
-    h1, h2, h3, h4 {
-        font-weight: 600 !important;
-        margin: 20px 0 5px !important;
-    }
-
-    h1 {
-        font-size: 22px !important;
-    }
-
-    h2 {
-        font-size: 18px !important;
-    }
-
-    h3 {
-        font-size: 16px !important;
-    }
-
-    .container {
-        width: 100% !important;
-    }
-
-    .content, .content-wrap {
-        padding: 10px !important;
-    }
-
-    .invoice {
-        width: 100% !important;
-    }
-}
-
-</style>
-
-<script type="text/javascript">
-
-</script>
-</body>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
