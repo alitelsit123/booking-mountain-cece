bac4b4fd6b4bb77aad11168094ba78be84da73cf
reza <reza.eduwork@gmail.com>
2022-10-02 04:13:38 +0700

    r

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Budeg Mountain</title>
+    <title>Budheg Mountain</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
