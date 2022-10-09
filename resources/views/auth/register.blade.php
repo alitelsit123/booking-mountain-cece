cc4c0a5cdecad770d3949a77f605789c4be57862
reza <reza.eduwork@gmail.com>
2022-10-09 17:45:13 +0700

    r

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-6 text-center mb-5">
-      <h2 class="heading-section">Daftar Akun Gunung Budek</h2>
+      <h2 class="heading-section">Daftar Akun Gunung Budheg</h2>
     </div>
   </div>
   <div class="row justify-content-center">
