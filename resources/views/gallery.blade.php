d49114d3ed80307a5c9110f4a37efb86605da2e3
mochrezaedu <reza.eduwork@gmail.com>
2022-11-03 06:26:11 +0700

    r

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -44,60 +44,57 @@
   </div> --}}
 
   <!-- Tab panes -->
-  <div class="tab-content">
-    <div id="info" class="container tab-pane active"><br>
-      <h4 style="font-weight: 900">Gambar</h4>
-      <!-- Gallery -->
-      <div class="row">
-        @foreach ($images as $row)
-        <div class="col-lg-4 col-md-12 mb-4 p-2">
-          <img style="
-          width: 100%;
-          height: 194px;
-          background-image: url('{{asset('storage/'.$row->url)}}');
-          background-repeat: no-repeat;
-          background-size: contain;
-          background-position: center;
-          cursor:pointer;
-          " class="btn-open-image-{{$row->id}}" data-target="#open-image-{{$row->id}}"></div>
-        </div>
-        <div id="open-image-{{$row->id}}" style="z-index:9999;position: fixed;top:0;left:0;display:none;width: 100vw;height:100vh;background-color: rgba(0, 0, 0, 0.473);">
-          <div style="position: relative;width:100%;">
-            <div style="position: absolute;top:10vh;left:50%;transform:translateX(-50%);">
-              <div style="position: relative;">
-                <div style="
-                width: 80vw;
-                height: 80vh;
-                background-image: url('{{asset('storage/'.$row->url)}}');
-                background-repeat: no-repeat;
-                background-size: contain;
-                background-position: center;
-                "></div>
-                <button class="btn btn-secondary" onclick="$('#open-image-{{$row->id}}').hide()" style="position: absolute;top:0.5rem;right:3.5rem;"><span class="fa fa-times"></span></button>
-              </div>
+  <div id="info" class="container tab-pane active"><br>
+    <h4 style="font-weight: 900">Gambar</h4>
+    <!-- Gallery -->
+    <div class="row">
+      @foreach ($images as $row)
+      <div class="col-lg-4 col-md-12 mb-4 p-2">
+        <div style="
+        width: 100%;
+        height: 194px;
+        background-image: url('{{asset('storage/'.$row->url)}}');
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center;
+        cursor:pointer;
+        " class="btn-open-image-{{$row->id}}" data-target="#open-image-{{$row->id}}"></div>
+      </div>
+      <div id="open-image-{{$row->id}}" style="z-index:9999;position: fixed;top:0;left:0;display:none;width: 100vw;height:100vh;background-color: rgba(0, 0, 0, 0.473);">
+        <div style="position: relative;width:100%;">
+          <div style="position: absolute;top:10vh;left:50%;transform:translateX(-50%);">
+            <div style="position: relative;">
+              <div style="
+              width: 80vw;
+              height: 80vh;
+              background-image: url('{{asset('storage/'.$row->url)}}');
+              background-repeat: no-repeat;
+              background-size: contain;
+              background-position: center;
+              "></div>
+              <button class="btn btn-secondary" onclick="$('#open-image-{{$row->id}}').hide()" style="position: absolute;top:0.5rem;right:3.5rem;"><span class="fa fa-times"></span></button>
             </div>
           </div>
         </div>
-        <script>
-          $('.btn-open-image-{{$row->id}}').click(function() {
-            $('#open-image-{{$row->id}}').show();
-          })
-        </script>
-        @endforeach
       </div>
-      <!-- Gallery -->
+      <script>
+        $('.btn-open-image-{{$row->id}}').click(function() {
+          $('#open-image-{{$row->id}}').show();
+        })
+      </script>
+      @endforeach
     </div>
+  </div>
 
-    <div id="book" class="container tab-pane active"><br>
-      <h4 style="font-weight: 900">Video</h4>
+  <div id="book" class="container tab-pane active"><br>
+    <h4 style="font-weight: 900">Video</h4>
 
-      <div class="row">
-        @foreach ($videos as $row)
-        <div class="col-md-6 page-yt-{{$row->id}}">
-          {!!$row->url!!}
-        </div>
-        @endforeach
+    <div class="row">
+      @foreach ($videos as $row)
+      <div class="col-md-6 page-yt-{{$row->id}}">
+        {!!$row->url!!}
       </div>
+      @endforeach
     </div>
   </div>
 
